419f89f2104cfce22ec14e4351eca8c4d48db154
Benno Dreißig <x3@x3cion.net>
2018-04-16 00:54:28 +0200

    Parser changes

--- dist/parser.js
+++ dist/parser.js
@@ -4,14 +4,13 @@ const stream_1 = require("stream");
 const string_decoder_1 = require("string_decoder");
 class Parser extends stream_1.Transform {
     constructor(opts) {
-        super(Object.assign(opts || {}, { objectMode: true }));
+        super(Object.assign(opts || {}, { readableObjectMode: true }));
         this.stringDecoder = new string_decoder_1.StringDecoder();
         this.colDelimiter = ";";
         this.colEscape = "\"";
         this.rowDelimiter = "\n";
         this.escapeChar = "\\";
         this.header = true;
-        this.headers = [];
         this.inColEscape = false;
         this.nextEscaped = false;
         this.currentData = "";
@@ -20,21 +19,33 @@ class Parser extends stream_1.Transform {
         this.currentDataMap = new Map();
         if (opts)
             Object.assign(this, opts);
-        // if (this.header) {
-        // 	this.workString = this.workStringHeader;
-        // }
+        if (this.header)
+            this.headers = [];
     }
-    _transform(data, encoding, next) {
-        this.workString(this.stringDecoder.write(data));
+    _transform(buff, encoding, next) {
+        const str = this.parseBuffer(buff);
+        this.workString(str);
         next();
     }
     _flush(done) {
+        const str = this.stringDecoder.end();
+        if (str) {
+            this.workString(str);
+        }
         this.finalizeCurrentFieldData();
         if (this.currentColumn === this.headers.length)
             this.push(this.currentDataMap);
         done();
     }
-    finalizeCurrentFieldData() {
+    getHeaders() {
+        return this.headers;
+    }
+    /**
+     * Writes gathered field data into final map. Adds the additional string if given.
+     * @param additionalString If set, will be added to the end of the data
+     */
+    finalizeCurrentFieldData(additionalString) {
+        additionalString && this.addData(additionalString);
         if (this.tanglingData)
             this.currentData = this.tanglingData + this.currentData;
         if (this.header)
@@ -43,19 +54,36 @@ class Parser extends stream_1.Transform {
             this.currentDataMap.set(this.headers[this.currentColumn++], this.currentData);
         this.currentData = "";
     }
-    workString(str) {
+    /**
+     * Decodes the given Buffer and attaches tangling data infront
+     * @param buf Buffer of utf8 data to be decoded
+     */
+    parseBuffer(buf) {
+        let str = this.stringDecoder.write(buf);
         if (this.tanglingData) {
             str = this.tanglingData + str;
             this.tanglingData = null;
         }
+        return str;
+    }
+    addData(str) {
+        this.currentData += str;
+    }
+    handleRowDelimiter({ str, i, last }) {
+        if (this.inColEscape || str.startsWith(this.rowDelimiter, i)) {
+            return false;
+        }
+        return true;
+    }
+    /**
+     * Parse string into output data structure
+     * @param str
+     */
+    workString(str) {
         let last = -1;
         for (let i = 0, l = str.length; i < l; i++) {
-            if (((this.rowDelimiter.length > 1 && str.length >= i + this.rowDelimiter.length && this.rowDelimiter === str.slice(i, i + this.rowDelimiter.length))
-                || str[i] === this.rowDelimiter) && !this.inColEscape) {
-                if (i - last > 1) {
-                    this.currentData = this.currentData.concat(str.slice(last + 1, i));
-                }
-                this.finalizeCurrentFieldData();
+            if (!this.inColEscape && str.startsWith(this.rowDelimiter, i)) {
+                this.finalizeCurrentFieldData(str.slice(last + 1, i));
                 if (!this.header) {
                     this.currentColumn !== this.headers.length && console.log("warning, got row delimiter, but column count not reached!", this.currentDataMap);
                     this.currentColumn = 0;
@@ -66,25 +94,22 @@ class Parser extends stream_1.Transform {
                     this.header = false;
                 last = i + this.rowDelimiter.length - 1;
             }
-            else if (str[i] === this.colDelimiter && !this.inColEscape) {
-                if (i - last > 1) {
-                    this.currentData = this.currentData.concat(str.slice(last + 1, i));
-                }
-                this.finalizeCurrentFieldData();
+            else if (!this.inColEscape && str[i] === this.colDelimiter) {
+                this.finalizeCurrentFieldData(str.slice(last + 1, i));
                 last = i;
             }
             else if (this.inColEscape
-                && ((this.colEscape === this.escapeChar && str[i] === this.colEscape && str.length > i + 1 && str[i + 1] === this.colEscape)
-                    || (this.colEscape !== this.escapeChar) && this.nextEscaped)) {
-                this.currentData = this.currentData.concat(str.slice(last + 1, i));
+                && ((this.colEscape !== this.escapeChar && this.nextEscaped) ||
+                    (this.colEscape === this.escapeChar && str[i] === this.colEscape && str.length > i + 1 && str[i + 1] === this.colEscape))) {
+                this.addData(str.slice(last + 1, i));
                 last = i;
             }
             else if (this.inColEscape && this.colEscape === this.escapeChar && !this.nextEscaped && str[i] === this.colEscape && str.length < i + 1) {
                 break;
             }
-            else if (str[i] === this.colEscape && !this.nextEscaped) {
+            else if (!this.nextEscaped && str[i] === this.colEscape) {
                 if (this.inColEscape) {
-                    this.currentData = this.currentData.concat(str.slice(last + 1, i));
+                    this.addData(str.slice(last + 1, i));
                     last = i;
                     this.inColEscape = false;
                 }
@@ -104,7 +129,7 @@ class Parser extends stream_1.Transform {
             || (this.inColEscape && this.colEscape === this.escapeChar && tanglingData[tanglingData.length - 1] === this.colEscape))
             this.tanglingData = tanglingData;
         else
-            this.currentData = this.currentData.concat(tanglingData);
+            this.addData(tanglingData);
     }
 }
 exports.default = Parser;
